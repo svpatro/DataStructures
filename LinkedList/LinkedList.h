d0202eccc1164075103db9f81846119d188f7d99
Shamik Patro <patroshamik@gmail.com>
2024-07-31 20:34:44 -0500

    Updated LinkedList

--- LinkedList/LinkedList.h
+++ LinkedList/LinkedList.h
@@ -2,6 +2,15 @@
 
 Author: Shamik V. Patro
 
+printList(): Print entire linked list.
+insertAtFront(): Inserting new Node at front of linked list.
+insertAtEnd(): Inserting new Node at the end of linked list.
+deleteHead(): 
+getNodeAt():
+getSize():
+insertAfter():
+deleteAfter():
+
 */
 
 #include <iostream>
@@ -35,37 +44,77 @@ class LinkedList{
 			}
 		}
 		
-		void insertAtFront(int val){
-
+		Node* insertAtFront(int val){
+			Node *newNode = new Node(val);
+			if (size == 0) {
+				head = tail = newNode;
+			}
+			else {
+				newNode->next = head;
+				head = newNode;
+			}
+			size++;
+			return newNode;
 		}
 
-		void insertAtEnd(){
-
-
+		Node* insertAtEnd(int val){
+			Node *newNode = new Node(val);
+			if (size == 0) {
+				head = tail = newNode;
+			} 
+			else {
+				tail->next = newNode;
+				tail = newNode;
+			}
+			size++;
+			return newNode;
 		}
 
 		void deleteHead(){
-
-
+			if (0 == size){
+				std::cout << "Cannot delete from an empty list" << std::endl;
+			}
+			else if (1 == size){
+				size--;
+				delete head;
+			}
+			else {
+				size--;
+				Node* tmp = head;
+				head = head->next;
+				delete tmp;
+			}
 		}
 
-		int getNodeAt(int pos){
-
-
+		Node* getNodeAt(int pos){
+			if (pos < 0 || pos >= size){
+				std::cout << "This position doesn't exist." << std::endl;
+				return NULL;
+			}
+			else {
+				Node *tmp = head;
+				for (int i = 0; i < pos; i++){
+					tmp = tmp->next;
+				}
+				return tmp;
+			}
 		}
 		
 		int getSize(){
 			return size;
 		}
 
-		void insertAfter(Node node, int val){
+		void insertAfter(Node *node, int val){
 
+			// TODO: Implement
 
 		}
 
 		void deleteAfter(Node node){
 
-			
+
+			// TODO: Implement
+
 		}
 
 };
